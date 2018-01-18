5fd2b092d8afe9dad0195a9fc9961cc380019baf
Andrei Gherghescu <g.andrei.n@gmail.com>
2018-01-18 12:48:41 +0100

    Finished up to example 35 of Chapter 2

--- chapter2_Soundex/Soundex.h
+++ chapter2_Soundex/Soundex.h
@@ -3,34 +3,10 @@
 
 #include <string>
 #include <map>
+#include <iostream>
 
 class Soundex {
  public:
-    std::string Encode(const std::string & word) const {
-        return ZeroPad(Head(word) + EncodeToDigits(word));
-    }
-
-    const size_t kMaxCodeLength{4};
-
- private:
-    std::string ZeroPad(const std::string & word) const {
-        auto zeros_needed = kMaxCodeLength - word.length();
-
-        return word + std::string(zeros_needed, '0');
-    }
-
-    std::string Head(const std::string & word) const {
-        return word.substr(0, 1);
-    }
-
-    std::string EncodeToDigits(const std::string & word) const {
-        if (word.length() > 1) {
-            return EncodeDigit(word[1]);
-        } else {
-            return "";
-        }
-    }
-
     std::string EncodeDigit(char letter) const {
         const std::map<char, std::string> encodings {
             {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
@@ -44,10 +20,68 @@ class Soundex {
 
         auto it = encodings.find(letter);
         if (it == encodings.end()) {
-            return "";
+            return kNotADigit;
         } else {
             return it->second;
         }
+    }
+
+    std::string Encode(const std::string & word) const {
+        return ZeroPad(UpperFirst(Head(word)) + EncodeToDigits(Tail(word)));
+    }
+
+    const size_t kMaxCodeLength{4};
+    const std::string kNotADigit{"*"};
+
+ private:
+    std::string ZeroPad(const std::string & word) const {
+        size_t max_encoding_length{4};
+        max_encoding_length = std::min(word.length(), max_encoding_length);
+        auto zeros_needed = kMaxCodeLength - max_encoding_length;
+        return word + std::string(zeros_needed, '0');
+    }
+
+    std::string UpperFirst(const std::string& encoding) const {
+        unsigned char first_letter = static_cast<unsigned char>(encoding.front());
+        return std::string(1, std::toupper(first_letter));
+    }
+
+    std::string Head(const std::string & word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string Tail(const std::string& word) const {
+        return word.substr(1, std::string::npos);
+    }
+
+    std::string EncodeToDigits(const std::string & word) const {
+        std::string encoding;
+        for (auto letter: word) {
+            if (IsCompleteEncoding(encoding)) {
+                break;
+            }
+            auto digit_encoding = EncodeDigit(letter);
+            if ( IsValidEncoding(digit_encoding) &&  digit_encoding!= GetLastEncodedDigit(encoding)) {
+                encoding += EncodeDigit(letter);
+            }
+        }
+        return encoding;
+    }
+
+    std::string GetLastEncodedDigit(const std::string& encoding) const {
+        if (encoding.empty()) {
+            return kNotADigit;
+        } else {
+            return std::string(1, encoding.back());
+        }
+    }
+
+    bool IsCompleteEncoding(const std::string & encoding) const {
+        return encoding.length() == kMaxCodeLength - 1;
+    }
+
+    bool IsValidEncoding(const std::string& encoding) const {
+        return encoding != kNotADigit;
     }
 };
 
--- chapter2_Soundex/SoundexTest.cpp
+++ chapter2_Soundex/SoundexTest.cpp
@@ -3,7 +3,7 @@
 #include "gmock/gmock.h"
 
 class SoundexEncoding : public testing::Test {
- public:
+public:
     Soundex soundex;
 };
 
@@ -19,6 +19,35 @@ TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigitCode) {
     ASSERT_THAT(soundex.Encode("Al"), testing::Eq("A400"));
 }
 
-TEST_F(SoundexEncoding, IgnoreNonAlphabeticsOrVowels) {
+TEST_F(SoundexEncoding, IgnoreNonAlphabetics) {
     ASSERT_THAT(soundex.Encode("A#"), testing::Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, IgnoreVowels) {
+    ASSERT_THAT(soundex.Encode("Ao"), testing::Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+    ASSERT_THAT(soundex.Encode("Acdl"), testing::Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+    ASSERT_THAT(soundex.Encode("Dcdlb").length(), testing::Eq(4u));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+    // Extra steps to clarify the same encoding for same type of consonants
+    ASSERT_THAT(soundex.EncodeDigit('b'), soundex.EncodeDigit('f'));
+    ASSERT_THAT(soundex.EncodeDigit('c'), soundex.EncodeDigit('g'));
+    ASSERT_THAT(soundex.EncodeDigit('d'), soundex.EncodeDigit('t'));
+
+    ASSERT_THAT(soundex.Encode("Abfcgdt"), testing::Eq("A123"));
+}
+
+TEST_F(SoundexEncoding, UppercasingTheFirstLetterInTheEncoding) {
+    ASSERT_THAT(soundex.Encode("abcd"), testing::StartsWith("A"));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+    ASSERT_THAT(soundex.Encode("BaAeEiIoOuUhHyYcdl"), testing::Eq("B234"));
 }
