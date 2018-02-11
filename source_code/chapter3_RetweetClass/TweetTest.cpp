6d777dc0f4cfe23440e1da285b3e3468101115c2
Andrei Gherghescu <g.andrei.n@gmail.com>
2018-02-11 20:18:47 +0100

    Refactored the InvalidUser exception class

--- source_code/chapter3_RetweetClass/TweetTest.cpp
+++ source_code/chapter3_RetweetClass/TweetTest.cpp
@@ -1,75 +1,79 @@
-#include "gmock/gmock.h"
 #include "Tweet.h"
+#include "gmock/gmock.h"
 
 TEST(ATweet, UserDefaultsToNullWhenNotProvided) {
-   Tweet tweet("msg");
+  Tweet tweet("msg");
 
-   ASSERT_THAT(tweet.GetUser(), ::testing::Eq("@null"));
+  ASSERT_THAT(tweet.GetUser(), ::testing::Eq("@null"));
 }
 
 TEST(ATweet, IsEqualToAnotherWhenMessageAndUserAreEqual) {
-   Tweet a("msg", "@user");
-   Tweet b("msg", "@user");
+  Tweet a("msg", "@user");
+  Tweet b("msg", "@user");
 
-   ASSERT_THAT(a, ::testing::Eq(b));
+  ASSERT_THAT(a, ::testing::Eq(b));
 }
 
 TEST(ATweet, IsNotEqualToAnotherWhenMessagesAreUnequal) {
-   Tweet a("msgX", "@user");
-   Tweet b("msgY", "@user");
+  Tweet a("msgX", "@user");
+  Tweet b("msgY", "@user");
 
-   ASSERT_THAT(a, ::testing::Ne(b));
+  ASSERT_THAT(a, ::testing::Ne(b));
 }
 
 TEST(ATweet, IsNotEqualToAnotherWhenUsersAreUnequal) {
-   Tweet a("msg", "@user1");
-   Tweet b("msg", "@user2");
+  Tweet a("msg", "@user1");
+  Tweet b("msg", "@user2");
 
-   ASSERT_THAT(a, ::testing::Ne(b));
+  ASSERT_THAT(a, ::testing::Ne(b));
 }
 
 TEST(ATweet, IsLessThanWhenMessagesAreEqualAndUserIsLessThan) {
-   Tweet a("msg", "@user1");
-   Tweet b("msg", "@user2");
+  Tweet a("msg", "@user1");
+  Tweet b("msg", "@user2");
 
-   ASSERT_THAT(a, ::testing::Lt(b));
+  ASSERT_THAT(a, ::testing::Lt(b));
 }
 
 TEST(ATweet, IsLessThanWhenUserEqualAndMessageIsLessThan) {
-   Tweet a("msgA", "@user");
-   Tweet b("msgB", "@user");
+  Tweet a("msgA", "@user");
+  Tweet b("msgB", "@user");
 
-   ASSERT_THAT(a, ::testing::Lt(b));
+  ASSERT_THAT(a, ::testing::Lt(b));
 }
 
 TEST(ATweet, CanBeCopyConstructed) {
-   Tweet a("msgA", "@user");
-   Tweet b(a);
+  Tweet a("msgA", "@user");
+  Tweet b(a);
 
-   ASSERT_THAT(a, ::testing::Eq(b));
+  ASSERT_THAT(a, ::testing::Eq(b));
 }
 
 class ATweetWithInvalidUser : public testing::Test {
-protected:
-  void SetUp() override {
-    invalid_user = std::string("notStartingWith@");
-  }
+ protected:
+  void SetUp() override { invalid_user = std::string("notStartingWith@"); }
   std::string invalid_user;
 };
 
-TEST_F(ATweetWithInvalidUser, RequiresUserToStartWithAnAtSign) {
-   ASSERT_ANY_THROW(Tweet tweet("msg", invalid_user));
-}
+TEST_F(ATweetWithInvalidUser, RequiresUserToStartWithAnAtSign) { ASSERT_ANY_THROW(Tweet tweet("msg", invalid_user)); }
 
 TEST_F(ATweetWithInvalidUser, RequiresUserToStartWithAnAtSignToBeValidUser) {
-   ASSERT_THROW(Tweet tweet("msg", invalid_user), InvalidUserException);
+  ASSERT_THROW(Tweet tweet("msg", invalid_user), InvalidUserException);
 }
 
 TEST_F(ATweetWithInvalidUser, RequiresUserNameToStartWithAnAtSign_v3) {
-   std::string invalidUser("notStartingWith@");
-   try {
-      Tweet tweet("msg", invalidUser);
-      FAIL();
-   }
-   catch (const InvalidUserException& /*expected*/) {}
+  try {
+    Tweet tweet("msg", invalid_user);
+    FAIL();
+  } catch (const InvalidUserException& /*expected*/) {
+  }
+}
+
+TEST_F(ATweetWithInvalidUser, RequiresUserNameToStartWithAtSignThrowsInvalidUserMessage) {
+  try {
+    Tweet tweet("msg", invalid_user);
+    FAIL();
+  } catch (const InvalidUserException& expected) {
+    ASSERT_STREQ("Invalid argument 'user': notStartingWith@", expected.what());
+  }
 }
